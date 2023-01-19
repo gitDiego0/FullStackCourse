c6d1a34e7d7440fdd4d46470b16f228ea54b9db6
Diego Garcia <you@example.com>
2023-01-19 20:03:06 +0100

    exercise 1.8

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header/Header'
 
-
+const Statistics = ({ good, neutral, bad, total }) => {
+  return (
+    <div>
+      <span>good {good}</span> <br />
+      <span>neutral {neutral}</span><br />
+      <span>bad {bad}</span><br />
+      <span>all {total}</span><br />
+      <span>average {(good + neutral - bad) / total}</span><br />
+      <span>positive {(good * 100) / total}%</span><br />
+    </div>)
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -13,34 +23,30 @@ const App = () => {
 
   const handleGood = () => {
     setGood(good + 1)
-    setTotal(total+1)
+    setTotal(total + 1)
   }
 
   const handleNeutral = () => {
-  setNeutral(neutral + 1)
-  setTotal(total+1)
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
   }
 
   const handleBad = () => {
-    setBad(bad +1)
-    setTotal(total +1)
+    setBad(bad + 1)
+    setTotal(total + 1)
   }
 
   return (
     <div>
       <Header title={'give feedback'} />
       <div >
-        <button  onClick={handleGood} >good</button>
-        <button  onClick={handleNeutral} >neutral</button>
-        <button  onClick={handleBad} >bad</button>
+        <button onClick={handleGood} >good</button>
+        <button onClick={handleNeutral} >neutral</button>
+        <button onClick={handleBad} >bad</button>
       </div>
       <Header title={'statics'} />
-      <span>good {good}</span> <br/>
-      <span>neutral {neutral}</span><br/>
-      <span>bad {bad}</span><br/>
-      <span>all {total}</span><br/>
-      <span>average {(good+neutral-bad)/total}</span><br />
-      <span>positive {(good *100)/total}%</span><br/>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} />
+
     </div>
   )
 }
