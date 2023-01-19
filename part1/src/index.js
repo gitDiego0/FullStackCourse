04210d7949e87548150ea7e23510804812c16eea
Diego Garcia <you@example.com>
2023-01-19 19:56:03 +0100

    exercise 1.7

--- part1/src/index.js
+++ part1/src/index.js
@@ -9,19 +9,38 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+    setTotal(total+1)
+  }
+
+  const handleNeutral = () => {
+  setNeutral(neutral + 1)
+  setTotal(total+1)
+  }
+
+  const handleBad = () => {
+    setBad(bad +1)
+    setTotal(total +1)
+  }
 
   return (
     <div>
       <Header title={'give feedback'} />
       <div >
-        <button  onClick={() =>setGood(prevGood => prevGood +1)} >good</button>
-        <button  onClick={() =>setNeutral(prevNeutral => prevNeutral +1)} >neutral</button>
-        <button  onClick={() =>setBad(prevBad => prevBad +1)} >bad</button>
+        <button  onClick={handleGood} >good</button>
+        <button  onClick={handleNeutral} >neutral</button>
+        <button  onClick={handleBad} >bad</button>
       </div>
       <Header title={'statics'} />
       <span>good {good}</span> <br/>
       <span>neutral {neutral}</span><br/>
       <span>bad {bad}</span><br/>
+      <span>all {total}</span><br/>
+      <span>average {(good+neutral-bad)/total}</span><br />
+      <span>positive {(good *100)/total}%</span><br/>
     </div>
   )
 }
