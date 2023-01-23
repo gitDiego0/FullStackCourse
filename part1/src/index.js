5d678c1b4ae6b64c6627ff1c917e9333642f6224
Diego Garcia <you@example.com>
2023-01-23 17:30:26 +0100

    exercise 1.10

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,18 +2,27 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header/Header'
 
-const Statistics = ({ good, neutral, bad, total }) => {
+// const Statistics = ({ good, neutral, bad, total }) => {
 
+//   return (
+//     <div>
+//       <span>good {good}</span> <br />
+//       <span>neutral {neutral}</span><br />
+//       <span>bad {bad}</span><br />
+//       <span>all {total}</span><br />
+//       <span>average {(good + neutral - bad) / total}</span><br />
+//       <span>positive {(good * 100) / total}%</span><br />
+//     </div>)
+// }
+
+const Statistics = ({ text, value }) => {
   return (
     <div>
-      <span>good {good}</span> <br />
-      <span>neutral {neutral}</span><br />
-      <span>bad {bad}</span><br />
-      <span>all {total}</span><br />
-      <span>average {(good + neutral - bad) / total}</span><br />
-      <span>positive {(good * 100) / total}%</span><br />
-    </div>)
+      <span>{text} {value}</span> <br />
+    </div>
+  )
 }
+
 
 const App = () => {
   // save clicks of each button to its own state
@@ -47,7 +56,15 @@ const App = () => {
       </div>
       <Header title={'statics'} />
       {
-        total === 0 ? 'No feedback given' : <Statistics good={good} neutral={neutral} bad={bad} total={total} />
+        total === 0 ? 'No feedback given' : <>
+
+          <Statistics text="good" value={good} />
+          <Statistics text="neutral" value={neutral} />
+          <Statistics text="bad" value={bad} />
+          <Statistics text="total" value={total} />
+          <Statistics text="average" value={(good - bad) / total} />
+          <Statistics text="positive" value={(good * 100) / total} />
+        </>
       }
 
 
