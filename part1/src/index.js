bbbea8f677cd98e71d951ac9b51b2a8f41408328
Diego Garcia <you@example.com>
2023-01-23 17:50:10 +0100

    exercise 1.12

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,81 +2,31 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header/Header'
 
-// const Statistics = ({ good, neutral, bad, total }) => {
 
-//   return (
-//     <div>
-//       <span>good {good}</span> <br />
-//       <span>neutral {neutral}</span><br />
-//       <span>bad {bad}</span><br />
-//       <span>all {total}</span><br />
-//       <span>average {(good + neutral - bad) / total}</span><br />
-//       <span>positive {(good * 100) / total}%</span><br />
-//     </div>)
-// }
-
-const Statistics = ({ text, value }) => {
-  return (
-
-    <tr>
-
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  )
+const randomizer = () => {
+  return Math.floor(Math.random() * 6)
 }
 
-
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-
-  const handleGood = () => {
-    setGood(good + 1)
-    setTotal(total + 1)
-  }
-
-  const handleNeutral = () => {
-    setNeutral(neutral + 1)
-    setTotal(total + 1)
-  }
-
-  const handleBad = () => {
-    setBad(bad + 1)
-    setTotal(total + 1)
-  }
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
 
   return (
     <div>
-      <Header title={'give feedback'} />
-      <div >
-        <button onClick={handleGood} >good</button>
-        <button onClick={handleNeutral} >neutral</button>
-        <button onClick={handleBad} >bad</button>
-      </div>
-      <Header title={'statics'} />
-      {
-        total === 0 ? 'No feedback given' : <table>
-          <tbody>
-
-
-            <Statistics text="good" value={good} />
-            <Statistics text="neutral" value={neutral} />
-            <Statistics text="bad" value={bad} />
-            <Statistics text="total" value={total} />
-            <Statistics text="average" value={(good - bad) / total} />
-            <Statistics text="positive" value={(good * 100) / total} />
-          </tbody>
-        </table>
-      }
-
-
+      <p>{props.anecdotes[selected]}</p>
+      <button onClick={() => setSelected(randomizer)}>next anecdote</button>
     </div>
   )
 }
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(<App anecdotes={anecdotes} />)
