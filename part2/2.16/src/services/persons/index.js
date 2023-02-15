9c7a79b32269e434460dca031d90870db1011d05
Diego Garcia <you@example.com>
2023-02-15 19:22:02 +0100

    exercise 2.16

---
+++ part2/2.16/src/services/persons/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export const getPersons = () => {
+  return axios.get('http://localhost:3001/persons')
+    .then(response => {
+      const { data } = response
+      return data
+    })
+}
+
+export const addPerson = ({ name, number }) => {
+  return axios.post('http://localhost:3001/persons', { name, number })
+    .then(response => {
+
+      return response.data
+    })
+}
